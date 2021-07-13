4ec70a7c7e0533a40f1d9f216c377a3c0200b392
bahngdaheun <reparo@naver.com>
2021-07-13 19:50:11 +0900

    .

---
+++ cinema2.sql
@@ -0,0 +1,11 @@
+create table cinema(
+title varchar(12) not null,
+mdate varchar(10) not null,
+time varchar(16) not null,
+price number(10) not null,
+seat varchar(8) not null,
+paydate varchar(10) not null
+);
+
+commit;
+desc cinema;
